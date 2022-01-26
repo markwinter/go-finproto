4af714133cb834972f0dfe7181ffae1b8aff186d
Mark Edward Winter <wintermarkedward@gmail.com>
2022-01-26 20:50:03 +0900

    Update example binary

--- cmd/itch/main.go
+++ cmd/itch/main.go
@@ -1,40 +1,48 @@
+/*
+ * Copyright (c) 2022 Mark Edward Winter
+ */
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
-	"os"
 
 	"github.com/markwinter/go-finproto/itch"
 )
 
 func main() {
+	filePath := flag.String("file", "", "Path to ITCH file")
+	flag.Parse()
+
 	config := itch.Configuration{
-		MessageTypes: []byte{},
-		MaxMessages:  0,
+		MessageTypes:   []byte{'S'},
+		MaxMessages:    0,
+		ReadBufferSize: itch.OneGB,
+	}
+
+	//defer profile.Start().Stop()
+	//defer profile.Start(profile.MemProfile).Stop()
+
+	messages, err := itch.ParseFile(*filePath, config)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	/*
-		messages, err := itch.ParseFile("01302020.NASDAQ_ITCH50", config)
+		// If you have a lot of memory or a small file then read the whole file into memory
+		data, err := os.ReadFile("01302020.NASDAQ_ITCH50")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		messages, err := itch.ParseMany(data, config)
 		if err != nil {
 			log.Fatal(err)
 		}
 	*/
 
-	data, err := os.ReadFile("01302020.NASDAQ_ITCH50")
-	if err != nil {
-		log.Fatal(err)
+	for _, message := range messages {
+		fmt.Printf("%v\n", message)
 	}
-
-	// defer profile.Start().Stop()
-
-	_, err = itch.ParseBytes(data, config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	/*
-		for _, message := range messages {
-			fmt.Printf("%v\n", message)
-		}
-	*/
 }
