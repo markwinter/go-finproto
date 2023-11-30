e6bd042e518d243272fb9823d8e2e35cd130b05a
Mark Winter <wintermarkedward@gmail.com>
2023-11-30 23:14:31 +0000

    Use go-cmp for tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/markwinter/go-finproto
 
 go 1.21
+
+require github.com/google/go-cmp v0.6.0
--- itch/5.0/system_event_test.go
+++ itch/5.0/system_event_test.go
@@ -3,13 +3,14 @@ package itch
 import (
 	"testing"
 	"time"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestMakeAndParse(t *testing.T) {
+func TestMakeAndParseSystemEvent(t *testing.T) {
 	loc, _ := time.LoadLocation("Europe/London")
 	now := time.Now()
 	midnight := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, loc)
-
 	timeSinceMidnight := now.Sub(midnight)
 
 	manual := SystemEvent{
@@ -21,31 +22,13 @@ func TestMakeAndParse(t *testing.T) {
 
 	s := MakeSystemEvent(timeSinceMidnight, 1, EVENT_START_HOURS)
 
-	if manual.EventCode != s.EventCode {
-		t.Errorf("EventCode incorrect, got: %s, want %s", s.EventCode, manual.EventCode)
-	}
-	if manual.StockLocate != s.StockLocate {
-		t.Errorf("EventCode incorrect, got: %d, want: %d", s.StockLocate, manual.StockLocate)
-	}
-	if manual.Timestamp != s.Timestamp {
-		t.Errorf("EventCode incorrect, got: %s, want: %s", s.Timestamp, manual.Timestamp)
-	}
-	if manual.TrackingNumber != s.TrackingNumber {
-		t.Errorf("EventCode incorrect, got: %d, want: %d", s.TrackingNumber, manual.TrackingNumber)
+	if !cmp.Equal(manual, s) {
+		t.Errorf("created event and manual struct not equal:\n%v\n%v", manual, s)
 	}
 
 	parsedEvent := ParseSystemEvent(s.Bytes())
 
-	if parsedEvent.EventCode != s.EventCode {
-		t.Errorf("EventCode incorrect, got: %s, want %s", parsedEvent.EventCode, s.EventCode)
-	}
-	if parsedEvent.StockLocate != s.StockLocate {
-		t.Errorf("EventCode incorrect, got: %d, want: %d", parsedEvent.StockLocate, s.StockLocate)
-	}
-	if parsedEvent.Timestamp != s.Timestamp {
-		t.Errorf("EventCode incorrect, got: %s, want: %s", parsedEvent.Timestamp, s.Timestamp)
-	}
-	if parsedEvent.TrackingNumber != s.TrackingNumber {
-		t.Errorf("EventCode incorrect, got: %d, want: %d", parsedEvent.TrackingNumber, s.TrackingNumber)
+	if !cmp.Equal(parsedEvent, s) {
+		t.Errorf("parsedEvent and created event not equal:\n%v\n%v", s, parsedEvent)
 	}
 }
