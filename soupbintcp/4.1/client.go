47b32ab4e992dc75332177aa07ad776b956148f5
Mark Winter <wintermarkedward@gmail.com>
2023-12-03 15:45:27 +0000

    move client options to a new file

--- soupbintcp/4.1/client.go
+++ soupbintcp/4.1/client.go
@@ -37,8 +37,6 @@ type Client struct {
 	backoff *backoff.ExponentialBackOff
 }
 
-type ClientOption func(client *Client)
-
 // NewClient creates a new soupbintcp client. The default parameters can be configured using ClientOptions passed in as parameters
 func NewClient(opts ...ClientOption) *Client {
 	b := backoff.NewExponentialBackOff(
@@ -64,51 +62,6 @@ func NewClient(opts ...ClientOption) *Client {
 	}
 
 	return c
-}
-
-// WithAuth sets the username and password to use when connecting to the Server
-func WithAuth(username, password string) ClientOption {
-	return func(c *Client) {
-		c.username = username
-		c.password = password
-	}
-}
-
-// WithCallback sets the callback function for every sequenced packet received
-func WithCallback(callback func([]byte)) ClientOption {
-	return func(c *Client) {
-		c.packetCallback = callback
-	}
-}
-
-// WithUnsequencedCallback sets the callback function for every unsequenced packet received
-func WithUnsequencedCallback(callback func([]byte)) ClientOption {
-	return func(c *Client) {
-		c.unsequencedCallback = callback
-	}
-}
-
-// WithDebugCallback sets the callback function for every debug packet received. Not normally used
-func WithDebugCallback(callback func(string)) ClientOption {
-	return func(c *Client) {
-		c.debugCallBack = callback
-	}
-}
-
-// WithServer sets the ip and port to use to connect to the Server
-func WithServer(ip, port string) ClientOption {
-	return func(c *Client) {
-		c.serverIp = ip
-		c.serverPort = port
-	}
-}
-
-// WithSession sets the initial session and sequence number when connecting to the Server
-func WithSession(id string, sequence uint64) ClientOption {
-	return func(c *Client) {
-		c.session = id
-		c.sequenceNumber = sequence
-	}
 }
 
 func (c *Client) connect() error {
